3ee8fe5d674603e60529a3f70546ea288efe7fcf
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-03-08 02:24:12 -0500

    feat (ui): added icons folder

---
+++ src/icons/AnonIcon.jsx
@@ -0,0 +1,22 @@
+
+export default function AnonIcon ({width, fill}) {
+  return (
+    <svg 
+      style={{
+          width,
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        style={{
+            fill,
+        }}
+        fillRule="evenodd"
+        d="M6.5 11h-2a.5.5 0 000 1h15a.5.5 0 000-1h-2a5.5 5.5 0 10-11 0zm1 0a4.5 4.5 0 019 0h-9zM10.437 15a2 2 0 100 1h3.126a2 2 0 100-1h-3.126zM8.5 14.5a1 1 0 100 2 1 1 0 000-2zm6 1a1 1 0 102 0 1 1 0 00-2 0z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+} 
